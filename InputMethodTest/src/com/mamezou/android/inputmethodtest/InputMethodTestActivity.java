b70d4bb94c028ba454ccfb71aa4723696d57f864
asanokouichi <asanokouichi@192.168.1.2>
2009-05-10 22:48:23 +0900

    ファーストコミット

---
+++ InputMethodTest/src/com/mamezou/android/inputmethodtest/InputMethodTestActivity.java
@@ -0,0 +1,13 @@
+package com.mamezou.android.inputmethodtest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class InputMethodTestActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
